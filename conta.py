37d28e713585c9f74b4d96e3ba3c4f2e801372e5
moniqueferreirasouza <monique.ferreira@aluno.ufabc.edu.br>
2022-03-14 02:25:10 -0300

    adicionando transferencia e comentarios do console

--- conta.py
+++ conta.py
@@ -1,26 +1,35 @@
-
-
 #Cria o endereço do objeto
 class Conta:
-    def __init__(self, numero, titular, saldo, limite):
+    def __init__(self, numero, titular, saldo, limite = 1000.0):
         print("Construindo objeto ...{}".format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero #__ serve para privar o atributo e não permitir sua edição fora do código (encapsular)
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
         #O self.xxx faz uma busca pelo valor declarado que está armazenado
 
     def extrato(self):
-        print("Saldo de {} do titular {}".format(self.saldo, self.titular))
+        print("Saldo de {} do titular {}".format(self.__saldo, self.__titular))
 
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def retira(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
 
+    def transfere(self, valor, destino):
+        self.retira(valor)
+        destino.deposita(valor)
 
+"""
+Aqui são os comandos utilizados no console do PyCharm para realizar as consultas
 
+from conta import Conta #informa o arquivo py e puxa a classe desejada
+conta = Conta (123, "Monique", 100.0, 500.0) #cria um novo objeto dentro da classe
+conta2 = Conta (321, "Joao", 100.0, 500.0) 
+conta._Conta__saldo #consulta o saldo
+conta.extrato() #extrato do saldo
+conta2.retira(20) #saque
+conta2.transfere(70.0, conta) #transferência
 
-
-
+"""
